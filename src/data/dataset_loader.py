a34ea55b27bf2b272138fa64e307b776632443fa
Vishnu Vandana Pyatla <vvandanapdev@gmail.com>
2026-07-19 14:40:45 -0500

    Loading the dataset and mapping

--- src/data/dataset_loader.py
+++ src/data/dataset_loader.py
@@ -1,5 +1,24 @@
+import os
+os.environ["HF_HOME"] = "D:/huggingface_cache"
 from datasets import load_dataset
 
 dataset = load_dataset("jtatman/python-code-dataset-500k", split="train")
 print(len(dataset))
-print(dataset[0])
+example = dataset[0]
+system_values = set(dataset["system"][:1000])
+print(len(system_values))
+
+def format_example_batched(batch):
+    texts = []
+    for instruction, output in zip(batch["instruction"], batch["output"]):
+        formatted = (
+            "Below is an instruction that describes a task. "
+            "Write a response that appropriately completes the request.\n\n"
+            f"### Instruction:\n{instruction}\n\n"
+            f"### Response:\n{output}"
+        )
+        texts.append(formatted)
+    return {"text": texts}
+
+dataset = dataset.map(format_example_batched, batched=True, batch_size=1000)
+print(dataset[0]["text"])

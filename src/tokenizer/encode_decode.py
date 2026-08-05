68ad1f64ec5b47016318b0d2528d17eb2cd5a2db
Vishnu Vandana Pyatla <vvandanapdev@gmail.com>
2026-08-04 19:11:06 -0500

    Add encode code

---
+++ src/tokenizer/encode_decode.py
@@ -0,0 +1,32 @@
+import json
+import re
+from pathlib import Path
+
+PATTERN = re.compile(r"[A-Za-z0-9_]+|[^\sA-Za-z0-9_]|\s+")
+
+
+def encode(text, merges, vocab, pattern):
+    chunks = pattern.findall(text)
+    token_ids = []
+
+    for chunk in chunks:
+        word = list(chunk)
+
+        for pair in merges:
+            i = 0
+            new_word = []
+            while i < len(word):
+                if i < len(word) - 1 and word[i] == pair[0] and word[i + 1] == pair[1]:
+                    new_word.append(pair[0] + pair[1])
+                    i += 2
+                else:
+                    new_word.append(word[i])
+                    i += 1
+            word = new_word
+
+        for token in word:
+            token_ids.append(vocab[token])
+
+    return token_ids
+
+
